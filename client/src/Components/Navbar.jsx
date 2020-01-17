bd400978f3f982b52cc7740222af995b631e2af2
Mano Sriram <mano.sriram0@gmail.com>
2020-01-17 19:05:55 +0530

    Resolve Navbar issue.

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,46 +1,49 @@
-import { useHistory } from "react-router-dom";
-import React from "react";
-import "./Sc2.css";
+import {useHistory} from 'react-router-dom';
+import React from 'react';
+import './Sc2.css';
 
 const Navbar = (props, props1) => {
-    let history = useHistory();
+  let history = useHistory();
 
-    const logout = async e => {
-        await fetch("/auth/logout");
-        history.push("/");
-    };
+  const logout = async e => {
+    await fetch('/auth/logout');
+    history.push('/');
+  };
 
-    return (
-        <div id="nav">
-        {props.createPost && (
-            <a href="/create-post" id="reddit">
+  return (
+    <div id="nav">
+      {props.createPost && (
+        <>
+          <a href="/create-post" id="reddit">
             Create-Post
-
-        &nbsp; &nbsp;
-            </a>
-        )}
-        {props.showPosts && (
-            <a href="/show-posts" id="reddit">
+          </a>
+          &nbsp; &nbsp;
+        </>
+      )}
+      {props.showPosts && (
+        <>
+          <a href="/show-posts" id="reddit">
             Home
-        &nbsp; &nbsp;
-            </a>
-        )}
-        <a id="reddit" href="/projects">
+          </a>
+          &nbsp; &nbsp;
+        </>
+      )}
+      <a id="reddit" href="/projects">
         Projects
-        &nbsp; &nbsp;
-        </a>
-        <a id="reddit" href="/about">
+      </a>
+      &nbsp; &nbsp;
+      <a id="reddit" href="/about">
         About
-        &nbsp; &nbsp;
+      </a>
+      &nbsp; &nbsp;
+      {props.logged && (
+        <a onClick={logout} id="reddit">
+          Logout
         </a>
-        {props.logged && (
-            <a onClick={logout} id="reddit">
-            Logout
-            </a>
-        )}
-        <footer id="footer">© Copyright 2019 by Mano Sriram </footer>
-        </div>
-);
+      )}
+      <footer id="footer">© Copyright 2019 by Mano Sriram </footer>
+    </div>
+  );
 };
 
 export default Navbar;
