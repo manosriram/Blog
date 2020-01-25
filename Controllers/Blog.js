9e239e97d30980a3ae706f4639610189a0057ecc
Mano Sriram <mano.sriram0@gmail.com>
2020-01-25 13:16:14 +0530

    Add Resume S3 link and fix Post-order-list.

--- Controllers/Blog.js
+++ Controllers/Blog.js
@@ -1,66 +1,66 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Blog = require("../Models/Blog");
+const Blog = require('../Models/Blog');
 
 router.delete('/delete-post', async (req, res) => {
-    const {postID} = req.body;
-    if (req.session.user) {
-        try {
-            const rex = await Blog.deleteOne({_id: postID});
+  const {postID} = req.body;
+  if (req.session.user) {
+    try {
+      const rex = await Blog.deleteOne({_id: postID});
 
-            return res.json({scs: true, msg: "Post deleted."});
-        } catch (er) {
-            return res.json({scs: false, msg: "Error occured."});
-        }
-    } else {
-        return res.json({scs: false, msg: "No Admin found."});
+      return res.json({scs: true, msg: 'Post deleted.'});
+    } catch (er) {
+      return res.json({scs: false, msg: 'Error occured.'});
     }
+  } else {
+    return res.json({scs: false, msg: 'No Admin found.'});
+  }
 });
 
 router.post('/get-post', async (req, res) => {
-    const {postID} = req.body;
-    try {
-        const post = await Blog.findOne({_id: postID});
-        return res.json({scs: true, postContent: post});
-    } catch (er) {
-        console.log(er);
-        return res.json({scs: false, msg: "Some error occured."});
-    }
+  const {postID} = req.body;
+  try {
+    const post = await Blog.findOne({_id: postID});
+    return res.json({scs: true, postContent: post});
+  } catch (er) {
+    console.log(er);
+    return res.json({scs: false, msg: 'Some error occured.'});
+  }
 });
 
 router.post('/create-post', async (req, res) => {
-    const {title, category} = req.body;
-    const content = req.body.content.cont;
-    let em;
+  const {title, category} = req.body;
+  const content = req.body.content.cont;
+  let em;
 
-    if (!content || !title || !category)
-        return res.json({scs: false, msg: "Fill all fields"});
+  if (!content || !title || !category)
+    return res.json({scs: false, msg: 'Fill all fields'});
 
-    if (req.session.user)
-        em = req.session.user.email;
-    else
-        return res.json({scs: false, msg: "Not Logged-In"});
+  if (req.session.user) em = req.session.user.email;
+  else return res.json({scs: false, msg: 'Not Logged-In'});
 
-    try {
-        const blg = new Blog({
-            title,
-            content,
-            category,
-            createdBy: em,
-            createdOn: Date.now()
-        });
+  try {
+    const blg = new Blog({
+      title,
+      content,
+      category,
+      createdBy: em,
+      createdOn: Date.now(),
+    });
 
-        blg.save();
-        return res.json({scs: true, msg: "Uploaded Post!"});
-    } catch (er) {
-        console.log(er);
-        return res.json({scs: false, msg: "Some error occured."});
-    }
+    blg.save();
+    return res.json({scs: true, msg: 'Uploaded Post!'});
+  } catch (er) {
+    console.log(er);
+    return res.json({scs: false, msg: 'Some error occured.'});
+  }
 });
 
 router.get('/show-posts', async (req, res) => {
-    const posts = await Blog.find({createdBy: "mano.sriram0@gmail.com"});
-    return res.json({posts: posts.reverse()});
+  const posts = await Blog.find({createdBy: 'mano.sriram0@gmail.com'}).sort({
+    createdOn: -1,
+  });
+  return res.json({posts});
 });
 
 module.exports = router;
--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./Sc2.css";
-import Navbar from "./Navbar";
+import React from 'react';
+import './Sc2.css';
+import Navbar from './Navbar';
 
 const About = () => {
   return (
@@ -8,9 +8,9 @@ const About = () => {
       <Navbar showPosts={true} Git={true} />
       <div id="container">
         <p>
-          Hello, My name is Mano Sriram and I'm and CS UnderGraduate currently
-          living in Vizag, India. I am passionate about Programming and solving
-          problems that makes people's lives better.
+          Hello, My name is Mano Sriram and I'm an CS Undergraduate currently
+          living in Vizag, India. I am passionate about Programming and I love
+          solving problems that makes people's lives better.
         </p>
         <p>
           I started this blog to help people by sharing my thoughts on Tech
@@ -18,12 +18,15 @@ const About = () => {
           problem-sets that I faced .
         </p>
         <p>
-          You can contact me via{" "}
-          <a href="mailto:mano.sriram0@gmail.com">Email</a> ,{" "}
+          You can contact me via{' '}
+          <a href="mailto:mano.sriram0@gmail.com">Email</a> ,{' '}
           <a href="https://www.linkedin.com/in/mano-sriram-866569166/">
             LinkedIN
-          </a>{" "}
+          </a>{' '}
           or <a href="https://www.quora.com/profile/Mano-Sriram-3">Quora</a>
+        </p>
+        <p>
+          <a href="https://blogbucket.s3.ap-northeast-2.amazonaws.com/Resume.pdf">Resume</a>
         </p>
       </div>
     </>
