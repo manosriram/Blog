2d56ef8454c785511aed289692f20a37f6e008da
Mano Sriram <mano.sriram0@gmail.com>
2020-05-07 20:02:30 +0530

    Fix No-Admin Bug

--- client/src/Components/Admin.jsx
+++ client/src/Components/Admin.jsx
@@ -5,22 +5,23 @@ import Button from "@material-ui/core/Button";
 import "./Sc.css";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Create from "./Create";
+import { useHistory } from 'react-router-dom';
 
 const Admin = () => {
   const [data, setData] = useState({});
-  const [home, setHome] = useState(false);
   const [msg, setMsg] = useState("");
   const [loading, isLoading] = useState(true);
+  const history = useHistory();
 
   const confirmStatus = async () => {
     const resp = await fetch("/auth/checkStat");
     const resp2 = await resp.json();
-    if (resp2.scs) setHome(resp2.scs);
+    if (resp2.scs) history.push('/');
+    isLoading(false);
   };
 
   useEffect(() => {
     confirmStatus();
-    isLoading(false);
   }, []);
 
   const handleSubmit = async e => {
@@ -34,15 +35,13 @@ const Admin = () => {
       body: JSON.stringify({ data })
     });
     const resp2 = await resp.json();
-    if (resp2.scs === true) setHome(resp2.scs);
+    if (resp2.scs === true) history.push('/');
     else setMsg(resp2.msg);
   };
 
   const handleChange = e => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
-
-  if (home) return <Create />;
 
   return (
     <>
