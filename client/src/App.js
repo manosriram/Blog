54690afc8adf67d05386cabd14fedbfccd0c6958
Mano Sriram <mano.sriram0@gmail.com>
2020-06-04 00:40:00 +0530

    Fix AD

--- client/src/App.js
+++ client/src/App.js
@@ -8,26 +8,40 @@ import About from "./Components/About";
 import Post from "./Components/Post";
 import Cold from "./Components/Cold";
 import "./App.css";
-import {Helmet} from 'react-helmet'
+import { Helmet } from "react-helmet";
 
 const App = () => {
     return (
         <>
-        <Helmet>
-            <title>Home | Mano Sriram</title>
-            <meta name="description" content="Mano Sriram" />
-        </Helmet>
-        <Router>
-            <Switch>
-                <Route path="/bl-admin"><Admin /></Route>
-                <Route path="/create-post"><Create /></Route>
-                <Route path="/projects"><Projects /></Route>
-                <Route path="/about"><About /></Route>
-                <Route path="/post/:postID/:postName"><Post /></Route>
-                <Route path="/cold-storage"><Cold /></Route>
-                <Route path="/"><Show /></Route>
-            </Switch>
-        </Router>
+            <Helmet>
+                <title>Home | Mano Sriram</title>
+                <meta name="description" content="Mano Sriram" />
+            </Helmet>
+            <Router>
+                <Switch>
+                    <Route path="/notAnAdminRoute">
+                        <Admin />
+                    </Route>
+                    <Route path="/create-post">
+                        <Create />
+                    </Route>
+                    <Route path="/projects">
+                        <Projects />
+                    </Route>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/post/:postID/:postName">
+                        <Post />
+                    </Route>
+                    <Route path="/cold-storage">
+                        <Cold />
+                    </Route>
+                    <Route path="/">
+                        <Show />
+                    </Route>
+                </Switch>
+            </Router>
         </>
     );
 };
