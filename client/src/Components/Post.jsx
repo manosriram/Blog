c63faff9d897d0d240004ce66cd6d1de773e08b3
Mano Sriram <mano.sriram0@gmail.com>
2020-11-27 21:52:24 +0530

    Fix Image width and height

--- client/src/Components/Post.jsx
+++ client/src/Components/Post.jsx
@@ -27,9 +27,13 @@ const Post = props => {
     const [CN, setCN] = useState("");
 
     const fetchUser = async () => {
-        const resp = await fetch("/auth/checkStat");
-        const data = await resp.json();
-        setLogStat(data.scs);
+        try {
+            const resp = await fetch("/auth/checkStat");
+            const data = await resp.json();
+            setLogStat(data.scs);
+        } catch (er) {
+            console.log(er);
+        }
     };
 
     const handleClickOpen = () => setOpen(true);
@@ -112,97 +116,92 @@ const Post = props => {
         return (
             <>
                 <Helmet>
-                    <head>
-                        <script
-                            data-ad-client="ca-pub-3914701687422003"
-                            async
-                            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-                        ></script>
-                        <link
-                            rel="apple-touch-icon"
-                            sizes="57x57"
-                            href="../../public/Assets/apple-icon-57x57.png"
-                        />
-                        <link
-                            rel="apple-touch-icon"
-                            sizes="60x60"
-                            href="../../public/Assets/apple-icon-60x60.png"
-                        />
-                        <link
-                            rel="apple-touch-icon"
-                            sizes="72x72"
-                            href="../../public/Assets/apple-icon-72x72.png"
-                        />
-                        <link
-                            rel="apple-touch-icon"
-                            sizes="76x76"
-                            href="../../public/Assets/apple-icon-76x76.png"
-                        />
-                        <link
-                            rel="apple-touch-icon"
-                            sizes="114x114"
-                            href="../../public/Assets/apple-icon-114x114.png"
-                        />
-                        <link
-                            rel="apple-touch-icon"
-                            sizes="120x120"
-                            href="../../public/Assets/apple-icon-120x120.png"
-                        />
-                        <link
-                            rel="apple-touch-icon"
-                            sizes="144x144"
-                            href="../../public/Assets/apple-icon-144x144.png"
-                        />
-                        <link
-                            rel="apple-touch-icon"
-                            sizes="152x152"
-                            href="../../public/Assets/apple-icon-152x152.png"
-                        />
-                        <link
-                            rel="apple-touch-icon"
-                            sizes="180x180"
-                            href="../../public/Assets/apple-icon-180x180.png"
-                        />
-                        <link
-                            rel="icon"
-                            type="image/png"
-                            sizes="192x192"
-                            href="../../public/Assets/android-icon-192x192.png"
-                        />
-                        <link
-                            rel="icon"
-                            type="image/png"
-                            sizes="32x32"
-                            href="../../public/Assets/favicon-32x32.png"
-                        />
-                        <link
-                            rel="icon"
-                            type="image/png"
-                            sizes="96x96"
-                            href="../../public/Assets/favicon-96x96.png"
-                        />
-                        <link
-                            rel="icon"
-                            type="image/png"
-                            sizes="16x16"
-                            href="../../public/Assets/favicon-16x16.png"
-                        />
-                        <link
-                            rel="manifest"
-                            href="../../public/Assets/manifest.json"
-                        />
-                        <meta
-                            name="msapplication-TileColor"
-                            content="#ffffff"
-                        />
-                        <meta
-                            name="msapplication-TileImage"
-                            content="../../public/Assets/ms-icon-144x144.png"
-                        />
-                        <meta name="theme-color" content="#ffffff" />
-                        <title>{postName} | Mano Sriram</title>
-                        <meta name="description" content={postName} />
-                    </head>
+                    <script
+                        data-ad-client="ca-pub-3914701687422003"
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                    ></script>
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="57x57"
+                        href="../../public/Assets/apple-icon-57x57.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="60x60"
+                        href="../../public/Assets/apple-icon-60x60.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="72x72"
+                        href="../../public/Assets/apple-icon-72x72.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="76x76"
+                        href="../../public/Assets/apple-icon-76x76.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="114x114"
+                        href="../../public/Assets/apple-icon-114x114.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="120x120"
+                        href="../../public/Assets/apple-icon-120x120.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="144x144"
+                        href="../../public/Assets/apple-icon-144x144.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="152x152"
+                        href="../../public/Assets/apple-icon-152x152.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="../../public/Assets/apple-icon-180x180.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="192x192"
+                        href="../../public/Assets/android-icon-192x192.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="../../public/Assets/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="96x96"
+                        href="../../public/Assets/favicon-96x96.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="../../public/Assets/favicon-16x16.png"
+                    />
+                    <link
+                        rel="manifest"
+                        href="../../public/Assets/manifest.json"
+                    />
+                    <meta name="msapplication-TileColor" content="#ffffff" />
+                    <meta
+                        name="msapplication-TileImage"
+                        content="../../public/Assets/ms-icon-144x144.png"
+                    />
+                    <meta name="theme-color" content="#ffffff" />
+                    <title>{postName} | Mano Sriram</title>
+                    <meta name="description" content={postName} />
                 </Helmet>
                 <Navbar createPost={true} />
                 <div id="container">
